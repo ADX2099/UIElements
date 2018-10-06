bee7588865e120056353126e35a145b94d17ed30
Adrian Aguilar <radagasth@gmail.com>
2018-10-06 13:35:15 -0500

    Files Upload

---
+++ app/src/main/java/uielements/adx2099/com/adxuielements/MainActivity.java
@@ -0,0 +1,13 @@
+package uielements.adx2099.com.adxuielements;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
